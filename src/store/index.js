179df5a7eeec36a00d0edbe8dacc1d8637ef0159
Alex C <ulisescastro2015@gmail.com>
2019-04-16 23:56:21 -0500

    Added user model

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,45 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import user from './modules/user';
-
 Vue.use(Vuex);
 
+const state = {
+  userData: localStorage.userData ? JSON.parse(localStorage.userData) || {}
+  // userRegionName: localStorage.userRegionName
+  //   || "",
+  // userRegionCode: localStorage.userRegionCode
+  //   || "",
+  // userCountrycode: localStorage.userCountryCode
+  //   || "",
+  // userCountryName: localStorage.userCountryName
+  //   || "",
+  // userToken: localStorage.userToken
+  //   || "",
+};
+
+const mutations = {
+  SAVE_USER_DATA(state, data) {
+    state.userData = data;
+
+    localStorage.userData = JSON.stringify(data);
+  }
+}
+
+const actions = {
+  async SAVE_USER_DATA({ commit }, payloadObj) {
+    await commit('SAVE_USER_DATA', payloadObj);
+  }
+}
+
+const getters = {
+  userData() {
+    return state.userData;
+  }
+}
+
 export default new Vuex.Store({
-  modules: [
-    user
-  ],
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state
+  mutations,
+  actions,
+  getters,
 });
---
+++ src/store/models/user.js
@@ -0,0 +1,13 @@
+import { Model } from '@vuex-orm/core';
+
+export default class User extends Model {
+  static entity = 'users';
+
+  static fields() {
+    return {
+      id: this.attr(null),
+      name: this.attr(''),
+      email: this.attr(''),
+    }
+  }
+}
