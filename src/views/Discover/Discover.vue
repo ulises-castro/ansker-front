70b4df7e22b576ed2ca12a023291375f926d790e
Alex C <ulisescastro2015@gmail.com>
2019-04-20 00:19:03 -0500

    Added styles for discover view

--- src/views/Discover/Discover.vue
+++ src/views/Discover/Discover.vue
@@ -1,11 +1,17 @@
 <template lang="html">
   <containerApp>
-    <section class="container is-fluid p-t-20">
+    <section class="container is-fluid p-t-20 height100">
       <header>
         <span class="logo">Ansker:)</span>
       </header>
-      <aside class="width100">
+      <aside class=""
+        :class="{'publish-button-float' : true}">
+        <icon scale="1.5" name="feather-alt" />
+      </aside>
+      <aside class="secrets-container width100">
+        <div class="secret" style="height: 100px">
 
+        </div>
       </aside>
     </section>
   </containerApp>
@@ -26,5 +32,23 @@ export default {
   color: $primary;
   font-weight: bold;
   font-family: 'Marck Script', cursive;
+}
+
+.publish-button-float {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  right: 20px;
+  bottom: 80px;
+  padding: 15px;
+  border-radius: 100%;
+  color: $primary;
+  background: white;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
+
+.secrets-container {
+
 }
 </style>
