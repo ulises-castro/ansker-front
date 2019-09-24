297fd8168d540a026b990f28bc14ef7b32d0179e
Alex C <ulisescastro2015@gmail.com>
2019-09-24 17:34:56 -0500

    Added install option

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,31 +2,45 @@
 
 import { register } from 'register-service-worker'
 
+const notifyUserAboutUpdate = worker => {
+  worker.postMessage({ action: 'skipWaiting' })
+}
+
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
+    ready() {
       console.log(
         'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
+          'For more details, visit https://goo.gl/AFskqB'
       )
     },
-    registered () {
+    registered() {
       console.log('Service worker has been registered.')
     },
-    cached () {
+    cached() {
       console.log('Content has been cached for offline use.')
     },
-    updatefound () {
+    updatefound() {
       console.log('New content is downloading.')
     },
-    updated () {
+    updated(registration) {
       console.log('New content is available; please refresh.')
+      notifyUserAboutUpdate(registration)
     },
-    offline () {
+    offline() {
       console.log('No internet connection found. App is running in offline mode.')
     },
-    error (error) {
+    error(error) {
       console.error('Error during service worker registration:', error)
     }
+  })
+
+  let refreshing
+
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    if (refreshing) return
+
+    window.location.reload()
+    refreshing = true
   })
 }
