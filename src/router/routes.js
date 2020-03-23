39ed4dd2b3e9741948314c1902d7e87368c80c37
Ulises Castro <ulisescastro2015@gmail.com>
2020-03-22 20:36:31 -0600

    Added functions to import layour and pages in routers file

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,25 @@
 
+const get = (folder, component) => import(`${folder}/${component}.vue`)
+
+const loadPage = (page) => {
+  return get('pages', page)
+}
+
+const loadLayout = (layout) => {
+  return get('layout', layout)
+}
+
 const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('pages/Index.vue') }
+  //   ]
+  // }
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    component: loadPage('Index')
   }
 ]
 
