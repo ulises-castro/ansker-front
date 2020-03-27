5fb63815d1b88b349071228ea1f9f85372fd53c9
Ulises Castro <ulisescastro2015@gmail.com>
2020-03-26 19:01:08 -0600

    Added applayout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="parent-container">
+    <!-- <HeaderApp :showBackButton="showBackButton"></HeaderApp> -->
+
+    <van-nav-bar
+      :title="title"
+      class="headerBar"
+      left-text="Regresar"
+      @click-left="handlerActionLeft"
+      :left-arrow="showBackButton"
+      >
+      <div v-if="!showBackButton" slot="left">
+        <van-icon
+          color="#49AFC4"
+          class="is-size-5"
+          info="30"
+          name="bullhorn-o"
+        />
+      </div>
+      <div
+        slot="right"
+      >
+        <van-icon
+          color="#49AFC4"
+          v-if="showBackButton"
+          class="is-size-5"
+          name="bullhorn-o"
+        />
+        <van-icon
+          color="#49AFC4"
+          v-else
+          @click="goPublish"
+          class="is-size-5" name="edit"
+        />
+      </div>
+
+      <span slot="title"> Ansker:) </span>
+      </van-nav-bar>
+    <slot></slot>
+    <Menu></Menu>
+  </section>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink'
+
+export default {
+  name: 'AppLayout',
+  data () {
+    return {
+    }
+  },
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    isFullPage: {
+      type: Boolean,
+      default: false,
+    },
+    showBackButton: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      default: '',
+    },
+  },
+}
+</script>
