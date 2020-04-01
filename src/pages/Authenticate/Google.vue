12aa56be0abd6a8bd50dc6995996bd25beb0bc45
Ulises Castro <ulisescastro2015@gmail.com>
2020-03-31 18:28:08 -0600

    ADded google auth

---
+++ src/pages/Authenticate/Google.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Loading data</div>
+</template>
+
+<script>
+import axios from "axios";
+import * as queryString from "query-string";
+
+export default {
+  name: "google",
+  methods: {
+    init() {
+      const urlParams = queryString.parse(window.location.search);
+
+      if (urlParams.error) {
+        console.log(`An error occurred: ${urlParams.error}`);
+      } else {
+        console.log(`The code is: ${urlParams.code}`);
+      }
+    }
+  },
+  mounted() {
+    this.init();
+  }
+};
+</script>
