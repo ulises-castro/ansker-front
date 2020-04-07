a5864aa74208368a3cb6071f157e3e6e3550119b
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-06 22:58:08 -0500

    Make request with auth token in order to get token and auth user

--- src/pages/Authenticate/AuthGoogle.vue
+++ src/pages/Authenticate/AuthGoogle.vue
@@ -3,35 +3,38 @@
 </template>
 
 <script>
-import axios from "axios";
-import AuthService from "src/services/auth.service";
-import * as queryString from "query-string";
+import axios from "axios"
+import AuthService from "src/services/auth.service"
+import * as queryString from "query-string"
 
 export default {
   name: "google",
   methods: {
     async init() {
-      const urlParams = queryString.parse(window.location.search);
-
-      console.log(`The code is: ${urlParams.code}`);
-
-      const [ err, googleData ] = await AuthService.googleLogin(urlParams.code);
-
-      console.log(err.response, googleData);
-      this.$notify(`${err.response.data.message}`);
-
-    },
-    getToken() {
       const urlParams = queryString.parse(window.location.search)
 
-      console.log(urlParams.accessToken)
-      // AuthService.getToken()
+      console.log(`The code is: ${urlParams.code}`)
+
+      const [ err, googleData ] = await AuthService.googleLogin(urlParams.code)
+
+      this.$notify(`${err.response.data.message}`)
+    },
+    getTokenAndsignIn() {
+      const { auth_token } = this.$route.params.auth_token
+      console.log(this.$route.params)
+      const [err, token] = AuthService.getApiToken(auth_token)
+
+      if (err)
     }
   },
   created() {
-    this.init();
+    if (this.$route.params.code) {
+      this.init()
+    }
+
+    this.getTokenAndsignIn()
   },
   mounted() {
   }
-};
+}
 </script>
