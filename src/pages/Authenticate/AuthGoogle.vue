f2bc9c22d2e5ba0b06fe552ecb342e386ea961ae
Ulises Castro <ulisescastro2015@gmail.com>
2020-04-07 20:05:46 -0500

    Sending real token and reciving api token in order to authenticate user

--- src/pages/Authenticate/AuthGoogle.vue
+++ src/pages/Authenticate/AuthGoogle.vue
@@ -20,11 +20,11 @@ export default {
       console.log(googleData)
       if (err) return this.$notify(`${err.response.data.message}`)
 
-      this.getTokenAndsignIn(googleData)
+      this.getTokenAndsignIn(googleData.data.access_token)
     },
-    getTokenAndsignIn(auth_token) {
+    getTokenAndsignIn(access_token) {
       console.log(this.$route.params)
-      const [err, token] = AuthService.getTokenAndSignIn(auth_token)
+      const [err, token] = AuthService.getTokenAndSignIn(access_token)
 
       if (err) return this.$notify(`${err.response.data.message}`)
 
