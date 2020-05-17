a51db06661d12df2a68aa2a0384836d160bb2557
Ulises Castro <ulisescastro2015@gmail.com>
2020-05-17 16:33:40 -0500

    Create a reutilizable share mixin

---
+++ src/mixins/share.js
@@ -0,0 +1,30 @@
+const ShareMixin = {
+  data: function () {
+    return {
+      propShowShare: false,
+      shareText: 'Comparte con personas de tu alrededor de forma anónima',
+    }
+  },
+  methods: {
+    share() {
+      this.choiceShare()
+    },
+    choiceShare() {
+      if (navigator.share) {
+        navigator.share({
+          title: this.shareText,
+          text: 'Comparte con tu alrededor anónimamente',
+          url: 'https://ansker.me'
+        })
+        .then(() => {
+          this.$notify({  type: 'success', message: 'Thanks for sharing!'});
+        })
+        .catch(console.error);
+      } else {
+        this.propShowShare = new Date()
+      }
+    },
+  }
+}
+
+export default ShareMixin
