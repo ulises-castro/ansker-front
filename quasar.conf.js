2fd72c9a3e2585cf3b7719c5a39dd2c10973db42
Ulises Castro <ulisescastro2015@gmail.com>
2020-05-24 13:59:47 -0500

    Added api

--- quasar.conf.js
+++ quasar.conf.js
@@ -72,7 +72,7 @@ module.exports = function (ctx) {
         SOCKET: JSON.stringify('https://localanskerme.me:3030'),
         BASE_URL: JSON.stringify('https://localanskerme.me:1297')
       } : { // and on build (production):
-        API: JSON.stringify('https://ansker.me/api/'),
+        API: JSON.stringify('https://api.ansker.me/'),
         GOOGLE_ID: JSON.stringify(
           '875317885894-lqjuife4ju82kas9rgks65nlnqk6ivdd.apps.googleusercontent.com'),
         SOCKET: JSON.stringify('https://ansker.me:3030'),
