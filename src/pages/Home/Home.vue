1e4cc6639c925b5442faa8d3b4fd49e7dbe3a6a3
Ulises Castro <ulisescastro2015@gmail.com>
2020-06-23 23:37:06 -0500

    Create join us components for create accounts

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -6,7 +6,7 @@
       </div>
       <p
         class="text-center slogan text-weight-bold text-white"
-      >Ansker es una red social Q&A, en donde puedes hacer preguntas y compartir lo que piensas con tu alrededor de manera anónima o publica.</p>
+      >Comparte lo que piensas con tu alrededor de manera anónima o publica.</p>
     </section>
     <section class="row justify-center q-px-sm">
       <van-button
@@ -14,9 +14,6 @@
         round
         type=" join-button bg-white text-primary text-weight-bold no-border q-mx-sm"
         size="large"
-        :loading="
-          auth.isLoading.google ||
-          auth.isLoading.facebook"
         loading-text="Iniciando sesión..."
       >
         <span class="text-h5 text-weight-bolder">Unirme</span>
@@ -27,133 +24,26 @@
         <u class="text-white text-weight-bold" style="font-size: 1.2em cursor-pointer">Ingresar</u>
       </div>
     </section>
-    <van-popup
-      round
-      position="bottom"
-      :style="{ height: '40%' }"
-      class="row wrap justify-center"
-      v-model="openJoinUs"
-      :overlay="true"
-    >
-      <div class="row items-center">
-        <span
-          class="logo-font text-primary text-weight-bolder text-center q-my-none q-py-sm text-h5"
-        >Ansker:)</span>
-      </div>
-      <small
-        class="full-width text-center text-weight-bold text-subtitle"
-        style="color: #363636"
-      >Comparte con tu alrededor de forma anónima</small>
-      <a
-        :href="googleLoginUrl"
-        class="full-width no-border bg-red-8 has-text-white row justify-center items-center cursor-pointer"
-      >
-        <span class="text-h6 text-white text-weight-bold">Continuar con Google</span>
-      </a>
-      <a
-        @click="checkLoginState"
-        class="full-width bg-blue-10 no-border has-text-white row justify-center items-center cursor-pointer"
-      >
-        <van-loading
-          v-if="auth.isLoading.facebook"
-          color="#fff"
-          size="1em"
-        />
-        <span class="text-h6 text-white text-weight-bold">Continuar con Facebook</span>
-      </a>
-      <aside class="q-py-sm row items-center">
-        <small class="text-weight-bold text-subtitle text-grey-9">
-          Al unirte, aceptas nuestros
-          <a
-            class="text-primary"
-            href="/terms"
-            target="_blanket"
-          >
-            Términos
-          </a> y
-          <a class="text-primary"
-          href="/policies" target="_blanket">
-            Políticas de privacidad
-          </a>
-        </small>
-      </aside>
-    </van-popup>
+
+    <joinUs :openJoinUs="openJoinUs"></joinUs>
+
     <section></section>
   </section>
 </template>
-
 <script>
-/* eslint-disable */
-import axios from 'axios'
-import { mapActions, mapGetters } from 'vuex'
-
-import User from 'src/services/UserService'
-
-import { Auth } from "src/services"
+import joinUs from 'components/JoinUs.vue'
 
 export default {
   name: "Home",
+  components: {
+    joinUs
+  },
   data() {
     return {
       showFooter: true,
       openJoinUs: false,
-      googleLoginUrl: Auth.getGoogleLink(),
-      auth: {
-        isLoading: {
-          facebook: false,
-          google: false
-        }
-      }
     }
   },
-  mounted() {
-  },
-  computed: {
-    ...mapGetters('User', [
-      'pushToken'
-    ])
-  },
-  methods: {
-    ...mapActions('User',[
-      'login'
-    ]),
-    checkLoginState() {
-      this.auth.isLoading.facebook = true
-
-      this.openLoginFB()
-    },
-    openLoginFB() {
-      FB.login((response) => {
-
-        if (response.authResponse) {
-          const tokenFB = response.authResponse.accessToken
-
-          this.loginWithFacebook(tokenFB)
-        } else {
-          this.auth.isLoading = false
-
-          this.$notify('Necesitamos permisos para poder continuar')
-        }
-      },{
-        scope: 'public_profile, email'
-      })
-    },
-    async loginWithFacebook(tokenFB) {
-      this.auth.isLoading.facebook = false
-
-      const [err, facebookUser] = await Auth.signInFacebook(tokenFB)
-
-      if (err || !facebookUser) return this.$notify('Ocurrio un error, intentalo más tarde')
-
-      this.$notify({ type: 'success', message: 'Bienvenido a Ansker ;)' })
-
-      this.login(facebookUser.data.token)
-
-      axios.defaults.headers.common['Authorization'] = `Bearer ${facebookUser.data.token}`
-
-      this.$router.push({ name: 'Discover' })
-    },
-  }
 }
 </script>
 <style lang="scss" scoped>
